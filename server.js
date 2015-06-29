4ea2e36522dfbc796849e6b22be8047c8776e7e1
respectYourNeighbour <philip_luca@yahoo.com>
2015-06-29 12:19:10 +0300

    documentation
    server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 /**************************************
 *************** Modules ***************
-*************************************** */
-var express        = require('express');
-var app            = express();
-var MongoClient    = require('mongodb').MongoClient;
+*** get a reference to the modules **** */
+
+var express        = require('express'); // Express is a library for NodeJS which handles for us a lot of routing and a lot of details of a web application.
+var app            = express(); // We create an instance of our app;
+var MongoClient    = require('mongodb').MongoClient; //This is the MongoDB Driver for NodeJS. It is a (JavaScript) library(module) that will handle the (BSON) protocol . We will use this driver's API to actually connect to MongoDB. This driver will do the communication with the database, the opening sockets, reading documents, detecting the errors, it will handle a lot of the things behind the scene so that we don't have to deal with them in our application code. 
+//** MongoClient - From the module 'mongodb' we get the MongoClient object. This object is our interface into the 'mongodb' driver library. Through this MongoClient object we will access the API methods to the driver wich will then handle whatever operation we requested.
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
@@ -13,7 +15,6 @@ var methodOverride = require('method-override');
 ********************************************* */
 var port = process.env.PORT || 8083; // We set the port on which our application will run.
 
-// We connect to our local database (in this case our db name is 'NMAstarterkit' - NodeMongoAngularStarterKit);
 MongoClient.connect('mongodb://localhost:27017/NMAstarterkit', function (err, db) {
     "use strict" //
     if(err) throw err;
@@ -27,7 +28,7 @@ MongoClient.connect('mongodb://localhost:27017/NMAstarterkit', function (err, db
 	app.use(express.static(__dirname + '/public')); // We set the static files location '/public'
 
 	// routes ==================================================
-	require('./routes')(app, db); // Pass our application into our routes;
+	require('./routes')(app, db); // We are passing our application into our routes;
 
 	// start app ===============================================
 	app.listen(port);	
@@ -36,3 +37,21 @@ MongoClient.connect('mongodb://localhost:27017/NMAstarterkit', function (err, db
 
 });
 
+
+/********************************************
+*************** Documentation ***************
+********************************************* */
+
+//*********** Database Connection *********** 
+
+// MongoClient.connect('mongodb://localhost:27017/NMAstarterkit', function (err, db) { ...
+
+// We connect to the Mongo Server and to our local database (in this case our db name is 'NMAstarterkit' - NodeMongoAngularStarterKit);
+// This will open the connection to the Server. Instead of checking the return value, we pass a callback function (function (err, db));
+// The first parameter in the callback function is to take the errors, and the second parameter is for the returned values.
+// The driver connects to the 'mongod' instance and it will call the callback function after it connects;
+// Because we dont check the return values, we need a way for the driver to tell us what happened and give us the results of this connection attempt;
+// If there is an error, the parameter 'err' will get populated. 
+// If the connection is succesful, the driver will call this callback that we passed in with the connected database object; 
+// The second argument 'db' is like a handle for our database connection. 
+// From now on, in this function (this callback)  we can use this database object ('db') to perform operations using this allready connected client.
